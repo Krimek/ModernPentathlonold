c2a9454bbacd9d5d59108fee8e7c46602ba6c321
Krzysztof Klekot <krzysztof.slawomir.klekot@gmail.com>
2016-12-13 00:22:57 +0100

    Dodano nowe okno i możliwość otwierania go

---
+++ ModernPentathlon/Windows/NewCompetition/FirstPageNewCompetition.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModernPentathlon.Windows.NewCompetition
+{
+    /// <summary>
+    /// Interaction logic for FirstPageNewCompetition.xaml
+    /// </summary>
+    public partial class FirstPageNewCompetition : Page
+    {
+        public FirstPageNewCompetition()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ModernPentathlon/Windows/NewCompetition/NewCompetition.xaml.cs
+++ ModernPentathlon/Windows/NewCompetition/NewCompetition.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ModernPentathlon
+namespace ModernPentathlon.Windows.NewCompetition
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for NewCompetition.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class NewCompetition : Window
     {
-        public MainWindow()
+        public NewCompetition()
         {
             InitializeComponent();
         }
