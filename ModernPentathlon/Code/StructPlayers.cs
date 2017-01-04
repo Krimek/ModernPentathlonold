7f3467cff91e2024ee2e644a919c723ef363b895
Krzysztof Klekot <krzysztof.slawomir.klekot@gmail.com>
2017-01-04 21:55:49 +0100

    Dodano klasę i metody odpowiedzialne za obsługę danych zawodników

---
+++ ModernPentathlon/Code/StructPlayers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernPentathlon.Code
+{
+    static class StructPlayers
+    {
+        static List<Player> ListOfPlayer = new List<Player>();
+
+        static void EditPlayer(int Id)
+        {
+
+        }
+        
+        static void EditPlayer(string name, string surname)
+        {
+
+        }
+
+        static void RemovePlayer(int id)
+        {
+            
+        }
+        static void RemovePlayer(string name, string surname)
+        {
+
+        }
+
+        static List<Player> GetMen()
+        {
+            List<Player> men = new List<Player>();
+            return men;
+        }
+
+        static List<Player> GetWomen()
+        {
+            List<Player> women = new List<Player>();
+            return women;
+        }
+    }
+}
