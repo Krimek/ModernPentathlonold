494f71f090fc35342dc1d70704a915b56394ce0f
Krzysztof Klekot <krzysiek.k366@wp.pl>
2017-02-01 02:57:49 +0100

    Dodano baze danych

---
+++ Database/PlayersData.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[PlayersData]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(20) NOT NULL, 
+    [Surname] VARCHAR(10) NOT NULL, 
+    [Sex] VARCHAR NOT NULL, 
+    [Club] TEXT NULL, 
+    [SwimTime] TIME NULL, 
+    [Birth] DATE NOT NULL
+)
